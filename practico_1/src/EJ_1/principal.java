5dcf9d59c5c64276c2bc2ed4f154d8c1776d5bef
LucasBubansRack <lucasbubans8@gmail.com>
2026-04-16 23:32:54 -0300

    segundo commit

--- practico_1/src/EJ_1/principal.java
+++ practico_1/src/EJ_1/principal.java
@@ -13,7 +13,7 @@ public class principal {
 
         Scanner sc = new Scanner(System.in);
         int num = 3;
-        int numero[] = new int[num];
+        int[] numero = new int[num];
         double suma = 0;
 
         System.out.println("INGRESO DE NUMEROS");
@@ -25,16 +25,32 @@ public class principal {
         }
 
         int numDiferent = 0;
-        for (int i = 0; i < 2; i++) {
-            if (numero[2] != numero[i]) {
-                numDiferent += 1;
+        for (int i = 1; i < numero.length; i++) {
+            if (numero[0] != numero[i]) {
+                numDiferent++;
+            }
+        }
+
+        int numMayor = numero[0], numMenor = numero[0];
+        for (int i = 0; i < 3; i++) {
+            if (numMayor < numero[i]) {
+                numMayor = numero[i];
+            } else if (numMenor > numero[i]) {
+                numMenor = numero[i];
             }
         }
 
         System.out.println("Numeros diferentes ingresados: " + numDiferent);
         System.out.println("Promedio: " + suma/num);
         System.out.println("Suma: " + suma);
+        System.out.println("El numero mayor ingresado es: " + numMayor);
+        System.out.println("El numero menor ingresado es: " + numMenor);
+        double producto = numMayor * numMenor;
+        System.out.println("Producto entre el mayor y el menor: " + producto);
 
+        if (producto % 3 == 0) {
+            System.out.println("El producto del Mayor por el Menor si es divisible por 3");
+        }
 
     }
 }
